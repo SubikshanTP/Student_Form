4e5b47b03ebad5a80ef7da03998cd78d078eda89
SubikshanTP <subishan01@gmail.com>
2025-12-05 21:18:57 +0530

    try

--- AutoMapper/StudentMapper.cs
+++ AutoMapper/StudentMapper.cs
@@ -3,11 +3,13 @@ using Student_Form.Model.Dto;
 using Student_Form.Model.Entity;
 
 namespace Student_Form.AutoMapper
-{
+{ 
     public class StudentMapper:Profile
     {
-       
-        
+
+       //subikshan
+   
+
         public StudentMapper()
         {
             CreateMap<StudentAdmissionDto,StudentAdmission>().ForMember(dest=>dest.Class,opt =>opt.MapFrom(src=>src.Classs)).ReverseMap();
