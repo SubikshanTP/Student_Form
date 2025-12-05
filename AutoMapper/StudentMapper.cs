55879ad8992f9b234dc69b8af254eab4d24b84ab
SubikshanTP <subishan01@gmail.com>
2025-12-05 13:45:35 +0530

    x

--- AutoMapper/StudentMapper.cs
+++ AutoMapper/StudentMapper.cs
@@ -3,11 +3,10 @@ using Student_Form.Model.Dto;
 using Student_Form.Model.Entity;
 
 namespace Student_Form.AutoMapper
-{
+{ 
     public class StudentMapper:Profile
     {
-        //-hbhbihbi
-        //njbhubiuh
+        
         public StudentMapper()
         {
             CreateMap<StudentAdmissionDto,StudentAdmission>().ForMember(dest=>dest.Class,opt =>opt.MapFrom(src=>src.Classs)).ReverseMap();
