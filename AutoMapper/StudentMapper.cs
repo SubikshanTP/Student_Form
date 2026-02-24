a0e514de14016c35a61fd72110d8ee71de9c7061
SubikshanTP <subishan01@gmail.com>
2026-02-24 11:54:37 +0530

    //vanakkam

--- AutoMapper/StudentMapper.cs
+++ AutoMapper/StudentMapper.cs
@@ -10,7 +10,7 @@ namespace Student_Form.AutoMapper
         public StudentMapper()
         {
             CreateMap<StudentAdmissionDto,StudentAdmission>().ForMember(dest=>dest.Class,opt =>opt.MapFrom(src=>src.Classs)).ReverseMap();
-            //i changed the name of the property in the StudentAdmissionDto to Classs because Class is a reserved keyword in C#. So, I mapped it to the Class property in the StudentAdmission entity.
+          // subikshan
         }
     }
 }
